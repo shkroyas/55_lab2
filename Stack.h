d3f49bfbb29aed871cbb85e89a1b97a861baeffa
Royas Shakya <shkroyas@gmail.com>
2024-05-12 04:56:51 +0100

    Stack Header file
    Define all the pure functions for Stack Implemetation  with constructor and destructor(read comments)

---
+++ Stack.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#ifndef  Stack_h
+#define Stack_h
+
+class Stack
+{
+public:
+    //callng a default constructor
+    Stack(){}
+    //a default destructror
+    ~Stack(){}
+    //pure virtual function for stack implementation
+    virtual void push(int item) = 0; 
+    virtual int pop() = 0;
+    virtual int top() = 0;
+    virtual bool isEmpty() = 0;
+};
+
+#endif /*Stack_h*/
+
