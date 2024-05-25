a7b60ae68e86dd89ba63a0df90decfbf1f23fa58
Royas Shakya <shkroyas@gmail.com>
2024-05-25 07:25:53 +0545

    Implemented pop() function for Stack.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
 #include "ArrayStack.h"
 
+using namespace std;
+
 int main() {
     ArrayStack stack(10); // Create a stack with capacity of 10
 
     if (stack.isEmpty()) {
-        std::cout << "Stack is empty" << std::endl;
+        cout << "Stack is empty" << endl;
     } else {
-        std::cout << "Stack is not empty" << std::endl;
+        cout << "Stack is not empty" << endl;
     }
     //pushing elements to stack
     stack.push(10);
     stack.push(20);
     stack.push(30);
     stack.push(40);
-    stack.push(50);
-    stack.push(60);
-    stack.push(70);
-    stack.push(80);
-    stack.push(90);
-    stack.push(92);
-    
 
     stack.printStack();// printing elements in the stack
+    //Follows the LIFO Principle of the stack 
+    stack.pop(); //Removes the Last element inserted
+    stack.pop(); //Removes the last element inserted
+    stack.pop(); //Removes the last element inserted
+    stack.pop(); //Removes the last element inserted
+    if (stack.isEmpty()) {
+        cout << "Stack is empty, no pop operation possible." << endl;
+    } else {
+        cout << "Stack is not empty" << endl;
+    }
 
-    stack.push(94);    //stack overflow 
+    stack.pop(); //Stack Underflow
+
+
 return 0;
 }
