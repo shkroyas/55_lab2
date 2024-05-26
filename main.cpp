07a1bf927e75ceaee63445e28a1ba8b4f8764933
Royas Shakya <shkroyas@gmail.com>
2024-05-26 07:05:08 +0545

    Implemented isEmpty() function for Stack in Linked List

--- main.cpp
+++ main.cpp
@@ -6,15 +6,13 @@ using namespace std;
 int main() {
     LinkedListStack stack; // Create a stack with capacity of 10
     // accessing top element of stack
-    stack.top();//Returns Stack is Empty
+    cout<<stack.isEmpty()<<endl;//Checking if the stack is empty
     //pushing elements to stack
     stack.push(10);
     stack.push(20);
     stack.push(30);
     stack.push(40);
 
-    stack.printStack();// printing elements in the stack
-    //accessing top element of stack
-    cout<<"The top element is "<< stack.top()<<endl;//Returns 40    
+    cout<<stack.isEmpty()<<endl;//Checking if the stack is empty
 return 0;
 }
