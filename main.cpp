119a3cbff082a670418f28390cdee4eef6ec4cc1
Royas Shakya <shkroyas@gmail.com>
2024-05-26 07:57:17 +0545

    Implemented enqueue(data) for Array in Queue

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ int main() {
     cout << "Checking if Array Queue is empty:\n";
     ArrayQueue queue(5);
     cout<<queue.isEmpty()<<endl;
+    queue.enqueue(10);
+    queue.enqueue(20);
+    queue.enqueue(30);
+    queue.printQueue(); //Prints all the elements in the queue
     
 return 0;
 }
